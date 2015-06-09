d8602c84056b24de5b045c55caca43f1c6f80562
pablozzz <gpvspb@gmail.com>
2015-06-09 19:26:50 +0300

    func find good

--- find_func_adress.py
+++ find_func_adress.py
@@ -1,17 +1,16 @@
 from ctypes import *
-
 kernel32  = windll.kernel32 
 
 #List of function names which adresses we find (example hasp-emulated functions)
 
-find_func = ["GetCommandLineA",
+func_list = ["GetCommandLineA",
              "GetProcAddress",
              "GetCurrentProcess",
              "GetStartupInfoA",
              "GetCurrentProcessId",
              "GetCurrentThreadId"]
 
-def func_adress_find(dll,function):
+def find_func_adress(dll,function):
 #Function input - dll name and name of function the return funtion's adress
     
         handle  = kernel32.GetModuleHandleA(dll)
@@ -20,8 +19,11 @@ def func_adress_find(dll,function):
         return address
 
 # loop for all functions
+def main(func_list):
+	for x in func_list:
+		printf_address = find_func_adress("kernel32.dll",x) #change dll name if need
+		print "[*] Address of %s: 0x%08x" % (x, printf_address)
 
-for x in find_func:
-    printf_address = func_adress_find("kernel32.dll",x) #change dll name if need
-    print "[*] Address of %s: 0x%08x" % (x, printf_address)
+if __name__ == '__main__': main(func_list)
+	
 
